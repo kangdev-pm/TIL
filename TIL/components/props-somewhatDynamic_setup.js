cc1e1b5a40c3c50225d9995824e05a9e62e722fe
kangheeyoung <kangdev@gmail.com>
2026-03-04 16:11:56 +0900

    props somewhatDynamic_setup
    여러 결과값으로 디자인에 랜더링

---
+++ TIL/components/props-somewhatDynamic_setup.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./css/index.css";
+
+const firstBook = {
+  author: "김승호",
+  title: "돈의 속성",
+  img: "../images/image-1.jpg",
+};
+
+const secondBook = {
+  author: "김호연",
+  title: "불편한 편의점",
+  img: "../images/image-2.jpg",
+};
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
+    </section>
+  );
+};
+
+const Book = (props) => {
+  // console.log(props);
+  return (
+    <article className="book">
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
